68ae85a9de5b2b2ee9f5083a1fdfd3ce7396ca33
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2023-08-11 17:20:12 -0300

    Adição de validação dos dtos

---
+++ app/src/main/java/com/simple_payment_aot/common/handlers/ValidationDto.java
@@ -0,0 +1,42 @@
+package com.simple_payment_aot.common.handlers;
+
+import java.util.Map;
+
+public class ValidationDto {
+    private String timestamp;
+    private Integer status;
+    private String error;
+    private Map<String, String> errors;
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+}
